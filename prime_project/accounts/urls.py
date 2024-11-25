a699186db27b328a2a227b1da5b69d10a06c3e09
Arian Hadi <arianhadi2003@gmail.com>
2024-11-26 01:07:40 +0300

    login end session feature

--- prime_project/accounts/urls.py
+++ prime_project/accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import SignUpView, EmailLoginView
+from .views import SignUpView, EmailLoginView,checkSessionView 
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
     path('login/', EmailLoginView.as_view() , name='login'),
+    path('check_session/',checkSessionView.as_view(), name = "check-session" )
 ]
--- prime_project/accounts/views.py
+++ prime_project/accounts/views.py
@@ -4,6 +4,10 @@ from django.views.generic.edit import CreateView
 from .forms import CustomUserCreationForm,EmailAuthenticationForm
 from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import View
 
 class SignUpView(CreateView):
     form_class = CustomUserCreationForm
@@ -18,6 +22,11 @@ class EmailLoginView(LoginView):
 
     def get_success_url(self):
         return reverse_lazy('home')
+
+class checkSessionView(LoginRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
+        return JsonResponse({"status" : "active"})
+
 
 
 
