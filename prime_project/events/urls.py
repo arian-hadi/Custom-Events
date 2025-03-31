77bbd9f942fb0719d1d108e38fc768c226567eeb
Arian Hadi <arianhadi2003@gmail.com>
2025-03-31 19:45:09 +0300

    fix: user-admin navbar fixed

--- prime_project/events/urls.py
+++ prime_project/events/urls.py
@@ -9,7 +9,7 @@ app_name = 'events'  # Namespace for event URLs
 urlpatterns = [
     path('', views.home, name='home'),
     path('events/', views.event_list, name='event_list'),
-    path('events/post/', views.post_event, name='create_event'),
+    path('post/', views.post_event, name='create_event'),
     path('events/<int:event_id>/', views.event_detail, name='event_detail'),
     path('events/<int:event_id>/apply/', views.apply_event, name='apply_event'),
 ]
