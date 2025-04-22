09b99ca68e172af5bd3ccb7f94654964ad393281
Arian Hadi <arianhadi2003@gmail.com>
2025-04-23 02:46:16 +0300

    update: privacy policy and terms of service included

--- prime_project/core/urls.py
+++ prime_project/core/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, TermOfServiceView, PrivacyPolicyView
 
 urlpatterns = [
     path("", HomeView.as_view(), name = 'home'),
+    path('terms/', TermOfServiceView.as_view(), name='terms'),
+    path('privacy/', PrivacyPolicyView.as_view(), name='privacy'),
+
 ]
--- prime_project/core/views.py
+++ prime_project/core/views.py
@@ -4,4 +4,11 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     template_name = "core/core.html"
+
+
+class TermOfServiceView(TemplateView):
+    template_name = "core/terms_of_service.html"
+
+class PrivacyPolicyView(TemplateView):
+    template_name = "core/privacy_policy.html"
     
