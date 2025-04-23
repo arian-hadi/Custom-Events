1a9487c775923c052c78f2446dcd507cb7e1fe62
Arian Hadi <arianhadi2003@gmail.com>
2025-04-23 03:34:36 +0300

    preparing for deployment

--- prime_project/prime_project/settings.py
+++ prime_project/prime_project/settings.py
@@ -25,7 +25,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core.apps.CoreConfig',
-
     'contact.apps.ContactConfig',
     'dashboard',
     'events',
